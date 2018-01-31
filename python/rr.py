Fix renko calculation

python/rr.py
@@ -38,8 +38,20 @@ class Renko:
         self.bdf = None
 
     def _validate_df(self):
+        self.df.reset_index(inplace=True)
+        self.df.columns = [i.lower() for i in self.df.columns]
         if not self.required_columns.issubset(self.df.columns):
             raise ValueError('DataFrame should have OHLC {} columns'.format(self.required_columns))
+
+    def uptrend_reversal(self, close):
+        lows = [self.cdf.iloc[i]['low'] for i in range(-1, -self.LINE_NUMBER - 1, -1)]
+        least = min(lows)
+        return close < least
+
+    def downtrend_reversal(self, close):
+        highs = [self.cdf.iloc[i]['high'] for i in range(-1, -self.LINE_NUMBER - 1, -1)]
+        highest = max(highs)
+        return close > highest
 
     def get_bricks(self):
         if self.chart_type == self.PERIOD_CLOSE:
@@ -47,42 +59,99 @@ class Renko:
         else:
             self.price_movement_bricks()
 
-        return self.bdf
+        return self.cdf
 
     def period_close_bricks(self):
         brick_size = self.brick_size
-        self.rdf = self.rdf[['date', 'close']]
 
-        self.rdf.loc[:, 'close_s1'] = self.rdf['close'] - self.rdf['close'].shift()
-        self.rdf.dropna(inplace=True)
+        self.df = self.df[['date', 'open', 'high', 'low', 'close']]
 
-        self.rdf.loc[:, 'close_r'] = (self.rdf['close'] // self.brick_size) * self.brick_size
-        self.rdf.loc[:, 'close_r_s1'] = (self.rdf['close_s1'] // self.brick_size) * self.brick_size
 
-        if debug:
-            print(self.rdf.head())
+        columns = ['date', 'open', 'high', 'low', 'close']
 
-        self.filter_noise()
-
-        bricks = self.rdf['bricks']
-        asign = np.sign(bricks)
-        self.rdf.loc[:, 'rtc'] = ((np.roll(asign, 1) - asign) != 0).astype(int)
-
-        self.rdf.loc[:, 'u_bricks'] = self.rdf.loc[self.rdf['rtc'] == 1, 'bricks']
-        self.rdf.loc[:, 'u_bricks'] = self.rdf['u_bricks'].apply(
-            lambda x: x - self.TREND_CHANGE_DIFF if x > 0 else x + self.TREND_CHANGE_DIFF
+        self.cdf = pd.DataFrame(
+            columns=columns,
+            data=[],
         )
-        self.rdf.loc[self.rdf['rtc'] == 0, 'u_bricks'] = self.rdf['bricks']
 
-        self.rdf = self.rdf[['close_r', 'u_bricks', 'date']]
-        self.rdf = self.rdf[self.rdf['u_bricks'] != 0]
-        self.rdf.reset_index(inplace=True)
-        self.rdf.dropna(inplace=True)
+        self.cdf.loc[0] = self.df.loc[0]
+        close = self.df.loc[0]['close'] // brick_size * brick_size
 
-        self.calculate_bricks_from_diff()
-        self.shift_bricks()
+        # print(self.cdf.loc[0][1:], [close - brick_size, close, close - brick_size, close, True])
+        self.cdf.loc[0, 1:] = [close - brick_size, close, close - brick_size, close]
 
-        self.bdf.reset_index(inplace=True, drop=True)
+        self.cdf['uptrend'] = True
+
+        # print(brick_size, close)
+        # print(self.cdf)
+        # print('=')
+
+        columns = ['date', 'open', 'high', 'low', 'close', 'uptrend']
+
+        for index, row in self.df.iterrows():
+
+            close = row['close']
+
+            row_p1 = self.cdf.iloc[-1]
+
+            uptrend = row_p1['uptrend']
+
+            open_p1 = row_p1['open']
+            high_p1 = row_p1['high']
+
+            low_p1 = row_p1['low']
+            close_p1 = row_p1['close']
+
+            date = row['date']
+
+            bricks = int((close - close_p1) / brick_size)
+
+            data = []
+
+            # print('========')
+            print(date, uptrend, bricks, close, close_p1)
+
+            if uptrend and bricks >= 1:
+                for i in range(bricks):
+                    r = [date, close_p1, close_p1 + brick_size, close_p1, close_p1 + brick_size, uptrend]
+                    data.append(r)
+                    close_p1 += brick_size
+                t = 'uc'
+            elif uptrend and bricks <= -2:
+                uptrend = not uptrend
+                bricks += 1
+                close_p1 -= brick_size
+                for i in range(abs(bricks)):
+                    r = [date, close_p1, close_p1, close_p1 - brick_size, close_p1 - brick_size, uptrend]
+                    data.append(r)
+                    close_p1 -= brick_size
+                t = 'ur'
+            elif not uptrend and bricks <= -1:
+                for i in range(abs(bricks)):
+                    r = [date, close_p1, close_p1, close_p1 - brick_size, close_p1 - brick_size, uptrend]
+                    data.append(r)
+                    close_p1 -= brick_size
+                t = 'dc'
+            elif not uptrend and bricks >= 2:
+                uptrend = not uptrend
+                bricks -= 1
+                close_p1 += brick_size
+                for i in range(abs(bricks)):
+                    r = [date, close_p1, close_p1 + brick_size, close_p1, close_p1 + brick_size, uptrend]
+                    data.append(r)
+                    close_p1 += brick_size
+                t = 'dr'
+            else:
+                continue
+
+            # print(data)
+            print(date, t, bricks, bricks <= -2, uptrend, close, close_p1)
+
+            sdf = pd.DataFrame(data=data, columns=columns)
+            self.cdf = pd.concat([self.cdf, sdf])
+
+        self.cdf.reset_index(inplace=True, drop=True)
+        return self.cdf
 
     def shift_bricks(self):
         shift = self.df['close'].iloc[-1] - self.bdf['close'].iloc[-1]
@@ -90,87 +159,6 @@ class Renko:
             return
         step = shift // self.brick_size
         self.bdf[['open', 'close']] += step * self.brick_size
-
-    def calculate_bricks_from_diff(self):
-        brick_size = self.brick_size
-
-        columns = ['open', 'close', 'date']
-        self.bdf = pd.DataFrame(
-            columns=columns,
-            data=[[0, 0, 0]],
-        )
-
-        prev_bricks = 1
-
-        cls = (self.df['close'].iloc[0] // brick_size) * brick_size
-
-        for index, row in self.rdf.iterrows():
-            bricks = row['u_bricks']
-            date = row['date']
-
-            data = []
-
-            for i in range(int(abs(bricks))):
-                if prev_bricks * bricks < 0 and i == 0 :
-                    cls = cls + brick_size * (bricks / abs(bricks))
-
-                r = [
-                    cls,
-                    cls + (brick_size * (bricks / abs(bricks))),
-                    date
-                ]
-                data.append(r)
-
-                cls = r[1]
-                prev_bricks = bricks
-
-            # print(data)
-            sdf = pd.DataFrame(data=data, columns=columns)
-
-            self.bdf = pd.concat([self.bdf, sdf])
-
-        return self.bdf
-
-
-    def filter_noise(self):
-        df = self.rdf
-        brick_size = self.brick_size
-
-        df.loc[:, 'cr_diff'] = df['close_r'] - df['close_r'].shift()
-        df = df[df['cr_diff'] != 0]
-        df.loc[:, 'bricks'] = df.loc[:, ('cr_diff', )] / brick_size
-        df.loc[:, 'bricks_s1'] = df['bricks'].shift()
-        df.loc[:, 'tc'] = np.where((df['bricks'] * df['bricks_s1']) < 0, True, False)
-
-
-        while True:
-            df.loc[:, 'cr_diff'] = df['close_r'] - df['close_r'].shift()
-            df = df[df['cr_diff'] != 0]
-
-            df['bricks'] = df.loc[:, ('cr_diff', )] / brick_size
-            df['bricks_s1'] = df['bricks'].shift()
-            df['tc'] = np.where((df['bricks'] * df['bricks_s1']) < 0, True, False)
-
-            filtered_df = df[(~df['tc']) | ~(abs(df['bricks']) == 1)]
-            if len(df) == len(filtered_df):
-                break
-            df = filtered_df
-
-
-        self.rdf = df
-
-
-def stock_df(symbol):
-    file = '{}'.format(symbol.upper())
-    if not os.path.exists(file):
-        print('Downloading {}'.format(symbol))
-        time.sleep(5)
-        quandl.ApiConfig.api_version = '2015-04-09'
-        df = quandl.get('NSE/{}'.format(symbol), start_date='2017-01-01', end_date='2018-01-25')
-        df.reset_index(inplace=True)
-        df.columns = [i.lower() for i in df.columns]
-        df.to_csv(file)
-    return pd.read_csv(file)
 
 
 if len(sys.argv) > 1:
@@ -196,6 +184,6 @@ df.columns = [i.lower() for i in df.columns]
 print(df.tail(20))
 
 renko = Renko(df)
-renko.brick_size = 4.5
+renko.brick_size = 2
 r = renko.get_bricks()
 print(r.tail(20))